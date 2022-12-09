c16faebfdb20cc4a5240338ea7ad4be8a9dfbf67
EC2 Default User <dachengjiana76@gmail.com>
2022-12-09 14:23:20 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navTaype : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : false,
+  autoPlayDuration : 500,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
